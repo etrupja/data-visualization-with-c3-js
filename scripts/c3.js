b6aeff0e9e53570239c32b4328d9c9d966caa7cc
Ervis Trupja <ervistrupja@outlook.com>
2020-05-09 15:55:17 +0200

    Revert "Updated"
    This reverts commit e281b6063ad63cba4af17073f533f634696b1192.

--- scripts/c3.js
+++
@@ -1,7 +0,0 @@
-function loadTimeLine(){
-    console.log('Load timeline called');
-}
-
-function loadSummary(){
-    console.log('Load summary called');
-}
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,7 @@
 $("#dashboardBtn").click(function () {
     $(this).text(function(i, text){
-        $('#worldwide').toggle();
-        $('#top5Countries').toggle();
-        if(text === "Worldwide"){
-            loadTimeLine();
-            return "Top 5 Countries";
-        } else {
-            loadSummary();
-            return "Worldwide";
-        }
+        $('#timeLine').toggle();
+        $('#summaryPie').toggle();
+        return text === "Timeline" ? "Summary" : "Timeline";
     })
 });
